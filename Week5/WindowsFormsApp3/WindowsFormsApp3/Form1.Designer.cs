20462f13af9ad41dc323ef795e5fc9b0a5698541
Yusuf Gön <gunes4122yy@gmail.com>
2025-10-11 14:30:30 +0300

    0.10 Week5 Part3

---
+++ Week5/WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button11 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MENÜ LİSTESİ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ÇORBALAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 52);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "PİLAVLAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 212);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 52);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "İÇECEKLER";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 285);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(147, 52);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "TATLILAR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(3, 368);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(101, 36);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "HEPSİNİ GÖSTER";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(3, 422);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(101, 36);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "TEMİZLE";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(182, 59);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(119, 52);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "EKLE";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(182, 132);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(119, 52);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "EKLE";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(182, 212);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(119, 52);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "EKLE";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(182, 285);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(119, 52);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "EKLE";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(355, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(145, 251);
+            this.listBox1.TabIndex = 11;
+            this.listBox1.Visible = false;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(540, 82);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(145, 251);
+            this.listBox2.TabIndex = 12;
+            this.listBox2.Visible = false;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(735, 82);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(145, 251);
+            this.listBox3.TabIndex = 13;
+            this.listBox3.Visible = false;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(907, 82);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(145, 251);
+            this.listBox4.TabIndex = 14;
+            this.listBox4.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(400, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Çorbalar";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(591, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Pilavlar";
+            this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(781, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "İçecekler";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(948, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Tatlılar";
+            this.label5.Visible = false;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(148, 383);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(119, 55);
+            this.button11.TabIndex = 19;
+            this.button11.Text = "RENK DEĞİŞTİR";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1083, 470);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ Week5/WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        DialogResult result;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            listBox2.Visible = false;
+            listBox3.Visible = false;
+            listBox4.Visible = false;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Visible = true;
+            label3.Visible = true;
+            listBox1.Visible = false;
+            listBox4.Visible = false;
+            listBox3.Visible = false;
+            label2.Visible=false;
+            label4.Visible = false;
+            label5.Visible = false;
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox3.Visible = true;
+            label4.Visible = true;
+            label2.Visible = false;
+            label3.Visible = false;
+            label5.Visible = false;
+            listBox2.Visible = false;
+            listBox4.Visible = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox4.Visible = true;
+            label5.Visible =true;
+            label2.Visible = false;
+            label3.Visible = false;
+            listBox2.Visible = false;
+            listBox3.Visible = false;
+            label4.Visible = false;
+        }
+        
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string corbaadi = Interaction.InputBox("Eklemek istediğiniz çorba adı", "Çorba Ekle", "", 500, 500);
+            result = MessageBox.Show(corbaadi +"Eklemek İstiyormusunuz?","",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+
+            if(result == DialogResult.Yes)
+            {
+                listBox1.Items.Add(corbaadi);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string pilavadi = Interaction.InputBox("Eklemek istediğiniz pilav adı", "Pilav Ekle", "", 500, 500);
+            result = MessageBox.Show(pilavadi + "Eklemek İstiyormusunuz?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                listBox2.Items.Add(pilavadi);
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string icecekadi = Interaction.InputBox("Eklemek istediğiniz İçecek adı", "İçecek Ekle", "", 500, 500);
+            result = MessageBox.Show(icecekadi + "Eklemek İstiyormusunuz?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                listBox3.Items.Add(icecekadi);
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            string tatliadi = Interaction.InputBox("Eklemek istediğiniz Tatlı adı", "Tatlı Ekle", "", 500, 500);
+            result = MessageBox.Show(tatliadi + "Eklemek İstiyormusunuz?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                listBox4.Items.Add(tatliadi);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Visible = true;
+            listBox2.Visible = true;
+            listBox3.Visible = true;
+            listBox4.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string temizleme = Interaction.InputBox("Hangi Kategoriyi Temizlemek İstersiniz", "Menü Silme", "", 500, 500);
+            switch (temizleme)
+            {
+                case "Çorbalar":listBox1.Items.Clear();break;
+                case "Pilavlar": listBox1.Items.Clear(); break;
+                case "İçecekler": listBox1.Items.Clear(); break;
+                case "Tatlılar": listBox1.Items.Clear(); break;
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            string renksecimi = Interaction.InputBox("Hangi Menünün Rengini Değiştirmek İstersiniz?", "Renk Değiştirme", "", 500, 500);
+            switch (renksecimi)
+            {
+                case "Çorbalar":colorDialog1.ShowDialog();listBox1.BackColor = colorDialog1.Color;break;
+                case "Pilavlar": colorDialog1.ShowDialog(); listBox2.BackColor = colorDialog1.Color; break;
+                case "İçecekler": colorDialog1.ShowDialog(); listBox3.BackColor = colorDialog1.Color; break;
+                case "Tatlılar": colorDialog1.ShowDialog(); listBox4.BackColor = colorDialog1.Color; break;
+
+            }
+        }
+    }
+}
