8fbc37ba9ceab060e4cb5fcef4f6da69d8558a02
Yusuf Gön <gunes4122yy@gmail.com>
2025-10-19 23:50:31 +0300

    0.14 Week6 Part2

---
+++ Week6/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public int toplam;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(Interaction.InputBox("Lütfen Bir Sayı Giriniz","Sayı Alma"," ",400,400));
+            if (sayi % 2 != 0)
+            {
+                listBox1.Items.Add(sayi);
+                toplam = toplam + sayi;
+            }
+            label2.Text = toplam.ToString();
+
+        }
+    }
+}
+//This Was a Quiz and i had 100.
---
+++ Week6/WindowsFormsApp2/WindowsFormsApp2/Form2.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 1;
+            while (i <= 100)
+            {
+                listBox1.Items.Add(i);
+                i++;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i = 100;
+            while (i >= 1)
+            {
+                listBox1.Items.Add(i);
+                i--;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = 1;
+            while(i <= 100)
+            {
+                if (i % 2 != 0)
+                {
+                    listBox1.Items.Add(i);
+                }
+                i++;
+            }
+            
+        }
+        public int baslangic, bitis;
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int i = 100;
+            while (i >= 1)
+            {
+                if (i % 2 == 0)
+                {
+                    listBox1.Items.Add(i);
+                }
+                i--;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+             baslangic= Convert.ToInt32(textBox1.Text);
+             bitis = Convert.ToInt32(textBox2.Text);
+
+            if (baslangic < bitis)
+            {
+                while (bitis >= baslangic)
+                {
+                    listBox1.Items.Add(baslangic);
+                    baslangic++;
+
+                }
+            }
+
+            else if(bitis < baslangic)
+            {
+                while(baslangic >= bitis)
+                {
+                    listBox1.Items.Add(baslangic);
+                    baslangic--;
+
+                }
+            }
+
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
